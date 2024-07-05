e66f4fbf5a0a365429f09316abbf178d87e38408
sergio-alegria <sergiioalegriia@gmail.com>
2024-07-05 13:37:21 +0200

    feat: stack navigation and tab navigation together

--- Navigation.js
+++ Navigation.js
@@ -1,59 +1,72 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
 //icons
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
 //screens
 import FormularioScreen from "./Screens/Formulario"
-import HomeScreen from "./Screens/Main"
+import HomeScreen from "./Screens/Home"
 import FestivalsScreen from "./Screens/Festivals"
+
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
 
 function MyTabs() {
   return (
-    <Tab.Navigator
-        initialRouteName='Home'
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: 'black',  
-          },
-        }
-          
-        }>
-      <Tab.Screen 
-      name="Formulario" 
-      component={FormularioScreen} 
-      options={{
-        tabBarLabel: 'Encuesta',
-        tabBarIcon: ({ color, size })=>(
-          <FontAwesome5 name="pencil-alt" size={24} color="white" />
-        ),
-      }}/>
-      <Tab.Screen name="Home" 
-      component={HomeScreen}
-      options={{
-        tabBarLabel: 'P.Principal',
-        tabBarIcon: ({ color, size })=>(
-          <FontAwesome5 name="home" size={24} color="red" />
-        ),
-      }} />
-      <Tab.Screen name="Festivals" 
-      component={FestivalsScreen}
-      options={{
-        tabBarLabel: 'Fiestas',
-        tabBarIcon: ({ color, size })=>(
-          <FontAwesome5 name="lemon" size={24} color="yellow" />
-        ),
-      }} />
+    <Tab.Navigator>
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="home" size={24} color="red" />
+          ),
+        }} />
+      <Tab.Screen
+        name="Formulario"
+        component={FormularioScreen}
+        options={{
+          tabBarLabel: 'Encuesta',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="pencil-alt" size={24} color="white" />
+          ),
+        }} />
+      <Tab.Screen name="Festivals"
+        component={FestivalsScreen}
+        options={{
+          tabBarLabel: 'Fiestas',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lemon" size={24} color="yellow" />
+          ),
+        }} />
     </Tab.Navigator>
   );
 }
 
-export default function Navigation(){
 
-  return(
+function StackNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}>
+      <Stack.Screen name="Main" component={MyTabs} />
+      <Stack.Screen name="Formulario" component={FormularioScreen} />
+      <Stack.Screen name="Festivales" component={FestivalsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+
+export default function Navigation() {
+  return (
     <NavigationContainer>
-      <MyTabs/>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 
-export default function Main() {
+export default function HomeScreen( { navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -13,7 +13,12 @@ export default function Main() {
         </View>
       </View>
       <View style={styles.container}>
-        <Text style={styles.subtitle}>Estás en Main</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Formulario")}>
+          <Text>Link to Formulario</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Festivales")}>
+          <Text>Link Festivales</Text>
+        </TouchableOpacity>
       </View>
       <ScrollView style={styles.largeContent}>
         <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
