e0cc70f23445f37fabfac5dfccd6ef1152fb1790
sergio-alegria <sergiioalegriia@gmail.com>
2024-07-05 18:13:48 +0200

    feat: extract FiestaComponent into a separate module

--- components/FiestaFlatList.js
+++ components/FiestaFlatList.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
+import FiestaComponent from './FiestaComponent';
+
 /**
  * componente FlatList que sirve para mostrar los datos que tenemos, la screen que use el componente
  * debe de ser la encargada de proporcionarlo los datos
@@ -8,42 +9,12 @@ import { StyleSheet, View, Text, Image, FlatList } from 'react-native';
  */
 export default function FlatListComponent({ data }) {
   return (
-    <FlatList 
-      data={data} 
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <View style={styles.itemContainer}>
-          <Image
-            source={{ uri: item.URL }}
-            style={styles.image}
-          />
-          <Text style={styles.title}>{item.title}</Text>
-        </View>
-      )} 
-    />
+    <View style={{flex: 1, flexGrow: 1}}>
+      <FlatList 
+        data={data} 
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={FiestaComponent} 
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  itemContainer: {
-    backgroundColor: "green",
-    marginBottom: 20,
-    alignItems: 'center',
-    marginVertical: 0,
-    marginHorizontal: 16,
-    paddingBottom: 30,
-    borderRadius: 6,
-  },
-  image: {
-    width: 400,
-    height: 300,
-    borderRadius: 6,
-    marginBottom: 10,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 10,
-    textAlign: 'center',
-  },
-});
