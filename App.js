c6e0c16f1cdda0ccc297860793e001a0e9edd355
anpeher <anpeher12@gmail.com>
2024-07-06 18:17:04 +0200

    comentarios y SafeAreaView

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 import 'react-native-gesture-handler';
 
 import Navigation from './Navigation';
-
+/**
+ * Aplicacion principal, su unico uso es el de cargar el componente Navigation que esel que se encargará de mostrar la pantalla principl
+ * y la navegacion entre ventanas.
+ */
 export default function App() {
   return (
     <Navigation />
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 /**
@@ -7,7 +8,7 @@ import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'rea
  */
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Image
           style={styles.logo}
@@ -28,7 +29,7 @@ export default function HomeScreen({ navigation }) {
       <ScrollView style={styles.largeContent}>
         <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</Text>
       </ScrollView>
-    </View>
+    </SafeAreaView>
   )
 }
 
